d6eef7a716f45e69ce0f9213e34906ea41b49970
Sam Byng <sam.byng@gmail.com>
2023-04-19 15:56:24 +0100

    Update test code

--- pekko-sample-cluster-docker-compose-scala/scripts/test.sh
+++ pekko-sample-cluster-docker-compose-scala/scripts/test.sh
@@ -6,19 +6,19 @@ sbt docker:publishLocal
 
 docker-compose up -d
 
-docker logs akka-sample-cluster-docker-compose-scala_seed_1
+docker logs pekko-sample-cluster-docker-compose-scala_seed_1
 
 for i in {1..10}
 do
   echo "Checking for MemberUp logging..."
-  docker logs akka-sample-cluster-docker-compose-scala_seed_1 | grep "Member is Up" | wc -l || true
-  [ `docker logs akka-sample-cluster-docker-compose-scala_seed_1 | grep "Member is Up" | wc -l` -eq 3 ] && break
+  docker logs pekko-sample-cluster-docker-compose-scala_seed_1 | grep "Member is Up" | wc -l || true
+  [ `docker logs pekko-sample-cluster-docker-compose-scala_seed_1 | grep "Member is Up" | wc -l` -eq 3 ] && break
   sleep 4
 done
 
 if [ $i -eq 10 ]
 then
   echo "No 3 MemberUp log events found:"
-  docker logs akka-sample-cluster-docker-compose-scala_seed_1
+  docker logs pekko-sample-cluster-docker-compose-scala_seed_1
   exit -1
 fi
