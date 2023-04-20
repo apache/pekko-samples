82b43d0dc2ee1a7e073d1bebfd9772768ae4443a
Sam Byng <sam.byng@gmail.com>
2023-04-20 10:02:06 +0100

    Update scripts

--- pekko-sample-cluster-kubernetes-scala/scripts/test.sh
+++ pekko-sample-cluster-kubernetes-scala/scripts/test.sh
@@ -9,7 +9,7 @@ kubectl config set-context docker-desktop
 
 kubectl apply -f kubernetes/namespace.json
 kubectl config set-context --current --namespace=appka-1
-kubectl apply -f kubernetes/akka-cluster.yml
+kubectl apply -f kubernetes/pekko-cluster.yml
 kubectl expose deployment appka --type=LoadBalancer --name=appka-service
 
 for i in {1..10}
