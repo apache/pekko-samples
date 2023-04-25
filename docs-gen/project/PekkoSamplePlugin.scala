0c205f7a111d872d1a88057b547ff44e072f9ca8
Sam Byng <sam.byng@gmail.com>
2023-04-25 16:31:04 +0100

    #35 : Use pekko paradox (#40)
    * Issue 35: Remove JDK 8 sbt-paradox plugin installation - we don't need it since we generate docs with JDK 11
    
    * Fix Issue 35: require PekkoParadoxPlugin instead of ParadoxPLugin
    
    * Split out different settings so that they're more easy to understand
    
    * Fix #35: Add themeSettings
    
    * Run scalafmt
    
    * Bump pekko-sbt-paradox to snapshot 0.0.0+39-1df580ce-SNAPSHOT
    
    ---------
    
    Co-authored-by: sb5 <sb5@ad.datcon.co.uk>

--- docs-gen/project/PekkoSamplePlugin.scala
+++ docs-gen/project/PekkoSamplePlugin.scala
@@ -1,10 +1,11 @@
 import sbt._
 import Keys._
-import com.lightbend.paradox.sbt.ParadoxPlugin
+import com.lightbend.paradox.sbt.ParadoxPlugin.autoImport._
 import org.apache.pekko.PekkoParadoxPlugin
+import org.apache.pekko.PekkoParadoxPlugin.autoImport._
 
 object PekkoSamplePlugin extends sbt.AutoPlugin {
-  override def requires = ParadoxPlugin
+  override def requires = PekkoParadoxPlugin
   override def trigger = allRequirements
   object autoImport {
     val baseUrl = settingKey[String]("")
@@ -14,10 +15,30 @@ object PekkoSamplePlugin extends sbt.AutoPlugin {
     val bodyTransformation = settingKey[String => String]("")
   }
   import autoImport._
-  import ParadoxPlugin.autoImport._
-  override def projectSettings: Seq[Setting[_]] = Seq(
-    baseUrl := "https://github.com/apache/incubator-pekko-samples/tree/main",
-    crossPaths := false,
+
+  val themeSettings = Seq(
+    // allow access to snapshots for pekko-sbt-paradox
+    resolvers += "Apache Nexus Snapshots".at("https://repository.apache.org/content/repositories/snapshots/"),
+    pekkoParadoxGithub := Some("https://github.com/apache/incubator-pekko"))
+
+  val propertiesSettings = Seq(
+    Compile / paradoxProperties ++= Map(
+      "download_url" -> s"https://example.lightbend.com/v1/download/${templateName.value}"))
+
+  val sourceDirectorySettings = Seq(
+    bodyPrefix := s"""${name.value}
+                     |=======================
+                     |
+                     |""".stripMargin,
+    // Transform local paths to URL
+    bodyTransformation := { case body =>
+      val r = """\[([^]]+)\]\(([^)]+)\)""".r
+      r.replaceAllIn(body,
+        _ match {
+          case r(lbl, uri) if !uri.contains("http") => s"""[$lbl](${baseUrl.value}/${baseProject.value}/$uri)"""
+          case r(lbl, uri)                          => s"[$lbl]($uri)"
+        })
+    },
     // Copy README.md file
     Compile / paradox / sourceDirectory := {
       val outDir = (Compile / managedSourceDirectories).value.head / "paradox"
@@ -29,19 +50,14 @@ object PekkoSamplePlugin extends sbt.AutoPlugin {
         IO.copyDirectory(inDir / "tutorial", outDir / "tutorial")
       }
       outDir
-    },
-    paradoxProperties += ("download_url" -> s"https://example.lightbend.com/v1/download/${templateName.value}"),
-    bodyPrefix := s"""${name.value}
-                     |=======================
-                     |
-                     |""".stripMargin,
-    bodyTransformation := { case body =>
-      val r = """\[([^]]+)\]\(([^)]+)\)""".r
-      r.replaceAllIn(body,
-        _ match {
-          case r(lbl, uri) if !uri.contains("http") => s"""[$lbl](${baseUrl.value}/${baseProject.value}/$uri)"""
-          case r(lbl, uri)                          => s"[$lbl]($uri)"
-        })
-    },
-    templateName := baseProject.value.replaceAll("-sample-", "-samples-"))
+    })
+
+  override def projectSettings: Seq[Setting[_]] =
+    themeSettings ++
+    propertiesSettings ++
+    sourceDirectorySettings ++
+    Seq(
+      baseUrl := "https://github.com/apache/incubator-pekko-samples/current",
+      crossPaths := false,
+      templateName := baseProject.value.replaceAll("-sample-", "-samples-"))
 }
