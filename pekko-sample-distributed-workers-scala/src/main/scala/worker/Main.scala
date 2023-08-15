e69d546ddc787bc336ed29311836862e335669ca
mario <mrenau@me.com>
2023-08-15 20:34:15 +0200

    Created WorkResultConsumerActor for handling published events

--- pekko-sample-distributed-workers-scala/src/main/scala/worker/Main.scala
+++ pekko-sample-distributed-workers-scala/src/main/scala/worker/Main.scala
@@ -2,12 +2,12 @@ package worker
 
 import java.io.File
 import java.util.concurrent.CountDownLatch
-
 import org.apache.pekko.actor.typed.ActorSystem
-import org.apache.pekko.actor.typed.scaladsl.Behaviors
+import org.apache.pekko.actor.typed.scaladsl.{ ActorContext, Behaviors }
 import org.apache.pekko.cluster.typed.Cluster
 import org.apache.pekko.persistence.cassandra.testkit.CassandraLauncher
 import com.typesafe.config.{ Config, ConfigFactory }
+import org.apache.pekko.actor.typed.eventstream.EventStream
 import org.apache.pekko.cluster.typed.SelfUp
 import org.apache.pekko.cluster.typed.Subscribe
 
@@ -39,6 +39,11 @@ object Main {
     }
   }
 
+  private def createWorkResultsConsumer(ctx: ActorContext[SelfUp]): Unit = {
+    val eventListener = ctx.spawn(WorkResultConsumerActor(), "work-consumer-actor")
+    ctx.system.eventStream ! EventStream.Subscribe(eventListener)
+  }
+
   def startClusterInSameJvm(): Unit = {
     startCassandraDatabase()
     // two backend nodes
@@ -57,6 +62,7 @@ object Main {
       Behaviors.setup[SelfUp](ctx => {
         val cluster = Cluster(ctx.system)
         cluster.subscriptions ! Subscribe(ctx.self, classOf[SelfUp])
+        createWorkResultsConsumer(ctx)
         Behaviors.receiveMessage {
           case SelfUp(_) =>
             ctx.log.info("Node is up")
